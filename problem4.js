d22e2c6e9e8540109605c0caa175b1371e601ab6
satish gupta <stishgupta5678@gmail.com>
2023-04-17 22:05:57 +0530

    comment

---
+++ problem4.js
@@ -0,0 +1,10 @@
+console.log("*****Result*****");
+
+
+console.log("Name     - Satish Kumar Gupta");
+console.log("School   - Masai School");
+console.log("Grade    - A");
+console.log("Section  - A");
+console.log("Roll no  - 1");
+console.log("Subjects - Maths     Science     English");
+console.log("Marks    -  92          95          99");
